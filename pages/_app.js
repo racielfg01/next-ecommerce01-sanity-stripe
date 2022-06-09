a92981f9ce9c1e912a543ff5105784067b71b459
Raciel Ferr <racielfg01@gmail.com>
2022-06-09 10:02:05 -0400

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+import '../styles/globals.css'
+
+import {Toaster} from 'react-hot-toast';
+import {Layout} from '../components/';
+import {StateContext} from '../context/StateContext';
+
+import React from 'react';
+
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <StateContext>
+    <Layout>
+      <Toaster/>
+      <Component {...pageProps} />
+    </Layout>
+    </StateContext>
+  )
+}
+
+
+export default MyApp
+
